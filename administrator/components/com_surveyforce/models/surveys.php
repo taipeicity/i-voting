93770901569491a4c669d863eb07adf5ce82a10c
臺北市政府資訊局i-Voting網路投票系統 <taipei@steps.com.tw>
2017-09-19 14:06:14 +0800

    v1.2

--- administrator/components/com_surveyforce/models/surveys.php
+++ administrator/components/com_surveyforce/models/surveys.php
@@ -2,7 +2,7 @@
 
 /**
 *   @package         Surveyforce
-*   @version           1.1-modified
+*   @version           1.2-modified
 *   @copyright       JooPlce Team, 臺北市政府資訊局, Copyright (C) 2016. All rights reserved.
 *   @license            GPL-2.0+
 *   @author            JooPlace Team, 臺北市政府資訊局- http://doit.gov.taipei/
