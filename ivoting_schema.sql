dc7e6eba9b3920135d851be3d49f2602726b16e8
臺北市政府資訊局i-Voting網路投票系統 <taipei@steps.com.tw>
2017-09-19 15:20:00 +0800

    v1.2

--- ivoting_schema.sql
+++ ivoting_schema.sql
@@ -1,19 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- 主機: localhost
--- 建立日期: Aug 22, 2017, 02:13 PM
--- 伺服器版本: 5.5.52
--- PHP 版本: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 
 
